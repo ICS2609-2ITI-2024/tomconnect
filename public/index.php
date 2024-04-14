c6608a5dc4873223c944ecc4794695a00d8b7e58
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-04-14 17:50:27 +0800

    updated index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 declare(strict_types=1);
 
-require_once dirname(__DIR__) . "/vendor/autoload.php";
+require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
 
-echo "Hello World";
+// WEBSITE LANDING PAGE HERE
