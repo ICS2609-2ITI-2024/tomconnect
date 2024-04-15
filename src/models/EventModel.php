f53b10083b58a6efdefd806ee430d600b0213c19
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-04-15 13:48:59 +0800

    added Models
    added model template

---
+++ src/models/EventModel.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tomconnect\Models;
+
+class EventModel extends Model
+{
+    // TODO: Create: Functions for creating new records/entities in the database or data store.
+
+    public function create()
+    {
+
+    }
+
+    // TODO: Read: Functions for retrieving data from the database or data store. These may include methods 
+    public function fetch_all()
+    {
+
+    }
+
+    public function fetch()
+    {
+
+    }
+
+    // TODO: Update: Functions for updating existing records/entities in the database or data store.
+
+    public function update()
+    {
+
+    }
+
+    // TODO Functions for deleting records/entities from the database or data store.
+
+    public function delete()
+    {
+        
+    }
+}
