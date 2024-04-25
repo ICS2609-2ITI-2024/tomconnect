412906454c90bebd0cab81b1512b0a252faaa79c
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-04-25 20:42:51 +0800

    added a 404 page

---
+++ public/404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TomConnect: 404</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <h1>404</h1>
+    <h3>Oops... Nothing to see here</h3>
+</body>
+</html>
