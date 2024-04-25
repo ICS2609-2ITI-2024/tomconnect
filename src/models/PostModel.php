cbe7dd7c28e9fb70b0cfed2df5e4bb5f14665109
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-04-25 20:01:04 +0800

    moved controllers and components

--- src/models/PostModel.php
+++ src/models/PostModel.php
@@ -8,32 +8,32 @@ class PostModel extends Model
 {
     // TODO: Create: Functions for creating new records/entities in the database or data store.
 
-    public function create(array $data)
+    public static function create(array $data)
     {
 
     }
 
     // TODO: Read: Functions for retrieving data from the database or data store. These may include methods 
-    public function fetch_all()
+    public static function fetch_all()
     {
 
     }
 
-    public function fetch($post_id)
+    public static function fetch($post_id)
     {
 
     }
 
     // TODO: Update: Functions for updating existing records/entities in the database or data store.
 
-    public function update($post_id, array $data)
+    public static function update($post_id, array $data)
     {
 
     }
 
     // TODO Functions for deleting records/entities from the database or data store.
 
-    public function delete($post_id)
+    public static function delete($post_id)
     {
         
     }
