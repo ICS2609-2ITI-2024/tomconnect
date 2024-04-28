374a2fcb7232e27eb517e2cb5d5d5ded28069e20
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-04-29 03:17:28 +0800

    Finished Creation of Sign Up handling

--- controllers/signup.php
+++ controllers/signup.php
@@ -8,51 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] !== "POST")
 
 require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
 
-use Tomconnect\Models\UserModel;
-
+use Tomconnect\Controllers\OrganizationSignUpForm;
 session_start();
 
-check_empty_username();
-check_empty_email();
-check_empty_password();
+$sign = new OrganizationSignUpForm();
 
-
-
-function check_empty_username()
-{
-    if (empty($_POST['username']) || !isset($_POST['username'])) {
-        $_SESSION['username_err'] = "Please enter a username";
-        return false;
-    }
-    return true;
-}
-
-function check_empty_password()
-{
-    if (empty($_POST['password1']) || !isset($_POST['password1'])) {
-        $_SESSION['password1_err'] = "Please enter a password";
-        return false;
-    }
-    if (empty($_POST['password2']) || !isset($_POST['password2'])) {
-        $_SESSION['password2_err'] = "Please enter a confirmed password";
-        return false;
-    }
-    return true;
-}
-
-function check_empty_email()
-{
-    if (empty($_POST['email']) || !isset($_POST['email'])) {
-        $_SESSION['email_err'] = "Please enter a email";
-        return false;
-    }
-    return true;
-}
-
-function sanitize_input($data)
-{
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+if ($sign->handle_field()) {
+    header("Location: " . "../public/signup_success.php");
+    die();
 }
