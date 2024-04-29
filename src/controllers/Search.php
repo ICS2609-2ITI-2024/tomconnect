da865226caebfe383c36b7a8df7f030827fac9fa
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-04-29 21:45:21 +0800

    initialize Search Controller

---
+++ src/controllers/Search.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Tomconnect\Controllers;
+
+use Tomconnect\Models\PostModel;
+use Tomconnect\Models\OrganizationModel;
+use Tomconnect\Models\TagModel;
+
+class search extends Controller
+{
+    
+}
