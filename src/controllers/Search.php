68fc24159250523821670602cd9dc69f05cf26c1
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-04-30 13:22:14 +0800

    updated Search Controller
    added new functions to search controller

--- src/controllers/Search.php
+++ src/controllers/Search.php
@@ -25,6 +25,37 @@ class search extends Controller
 
     private function search_organizations()
     {
-        // return OrganizationModel::search();
+        return OrganizationModel::search($_GET['q']);
+    }
+
+    private function remove_duplicates()
+    {
+        $hash_map = [];
+
+        foreach($this->posts as $post) {
+            $hash_map[$post['post_id']] = $post;
+        }
+
+        $this->posts = array_values($hash_map);
+    }
+
+    private function set_posts()
+    {
+        foreach(PostModel::search($_GET['q']) as $post) {
+            $this->posts[] = $post;
+        }
+
+        foreach($this->org_ids as $org_id) {
+            foreach(PostModel::search_from_column('author_id', $org_id) as $post) {
+                $this->posts[] = $post;
+            }
+        }
+    }
+
+    private function set_org_ids()
+    {
+        foreach($this->search_organizations() as $org) {
+            $this->org_ids[] = $org['org_id'];
+        }
     }
 }
