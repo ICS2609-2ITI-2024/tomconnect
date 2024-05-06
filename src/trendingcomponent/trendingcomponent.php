186efb4eb4d668cadb61fb30ef3d6dbad76811cc
Lec <alecgabriel.estrera.cics@ust.edu.ph>
2024-05-07 01:16:53 +0800

    Started the component

---
+++ src/trendingcomponent/trendingcomponent.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap and CSS Link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="trendstyles.css">
+    <title>TrendComponent</title>
+</head>
+
+<body>
+    <div class="row row-cols-1 row-cols-md-3 g-2 py-1">
+        <div class="col">
+            <div class="card cardstyle">
+                <h3 class="card-title">Trending Topics</h3>
+                <br>
+                <h5 class="trend">#TIGER</h5>
+                <table class="stats">
+                    <td>313 Posts</td>
+                    <td>100,000 Likes</td>
+                </table>
+                <br>
+                <h5 class="trend">#YELLOW</h5>
+                <table class="stats">
+                    <td>938 Posts</td>
+                    <td>8273 Likes</td>
+                </table>
+                <br>
+                <h5 class="trend">#GOUSTE</h5>
+                <table class="stats">
+                    <td>7362 Posts</td>
+                    <td>847,361 Likes</td>
+                </table>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
