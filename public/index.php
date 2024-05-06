27567377e0864d00e543aa526e6eb926416dfe9f
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-05-06 13:48:02 +0800

    sample post index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Tomconnect\Models\PostModel;
+
 require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
 
 session_start();
@@ -19,13 +21,39 @@ if (!isset($_SESSION['is_logged_in'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/main.css">
     <title>TomConnect</title>
+    <style>
+        .post-container {
+            display: flex;
+            flex-direction: column;
+            margin: 2rem;
+            gap: 1rem;
+        }
+
+        .post {
+            border: solid 1px black;
+            border-radius: 1rem;
+            padding: 2rem;
+        }
+
+    </style>
 </head>
 <body>
     <form action="../controllers/post.php" method="post" enctype="multipart/form-data">
-        <textarea name="post_content" id="post_content" cols="30" rows="10">
+        <textarea name="content" id="content" cols="30" rows="10">
         </textarea>
-        <input type="file" name="img" id="img">
+        <input type="file" name="upload" id="upload">
         <button type="submit">Post</button>
     </form>
+
+    <div class="post-container">
+        <?php foreach(PostModel::search_from_column('author_id', '3') as $post): ?>
+            <article class="post">
+                <div class="content">
+                    <?= $post['content'] ?>
+                </div>
+                <img src=<?= $post['media_url'] ?> alt="">
+            </article>
+        <?php endforeach ?>
+    </div>
 </body>
 </html>
