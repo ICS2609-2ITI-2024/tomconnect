39ff26f240679c50ae0056121f4f33823b9c1f3f
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-05-08 12:25:48 +0800

    added landing page background image

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Tomconnect\Components\EventCard;
 use Tomconnect\Models\PostModel;
 use Tomconnect\Components\OrgRegistrationButton;
+use Tomconnect\Models\OrganizationModel;
 
 require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
 
@@ -20,8 +21,27 @@ session_start();
     <title>TomConnect</title>
 </head>
 <body>
-    <div class="">
-        <?php EventCard::render() ?>
+    <img class="back-img" src="./img/testbg 2.svg" alt="">
+    <div class="left">
+        <h1>Left</h1>
     </div>
+
+    <main class="main" style="">
+        <h1>Feed</h1>
+        <?php foreach(PostModel::fetch_all() as $post): ?>
+            <article class="post">
+                <h3 class="author"><?= OrganizationModel::fetch($post['author_id'])['name'] ?></h3>
+                <p class="content"><?= $post['content'] ?></p>
+                <?php if ($post['media_url'] != null): ?>
+                    <img src=<?= $post['media_url'] ?> alt="">
+                <?php endif ?>
+            </article>
+        <?php endforeach ?>
+    </main>
+    <div class="right">
+        <h1>Right</h1>
+
+    </div>
+    <script src="./js/script.js"></script>
 </body>
 </html>
