52b9fac1077abb9771e57872e30736668043c047
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-05-11 15:50:51 +0800

    added navbar styles

--- components/header.php
+++ components/header.php
@@ -15,7 +15,7 @@ class Header
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= ucwords($page_title) ?></title>
         <link rel="shortcut icon" type="image/x-icon" href="./assets/logo.ico">
-        <link rel="stylesheet" href="../css/main.css">
+        <link rel="stylesheet" href="./css/main.css">
 </head>
 <body>
         <?php
--- public/index.php
+++ public/index.php
@@ -15,5 +15,24 @@ use Tomconnect\Components\NavbarComponent;
 
 Header::render('tomconnect');
 NavbarComponent::render();
+?>
+<img src="./img/testbg 2.svg" alt="" class="back-img">
+<div class="main-feed">
+    <div class="left">
+        Left
+    </div>
+    <main class="main">
+        <?php
+        foreach (PostModel::fetch_all() as $post) {
+            $author = OrganizationModel::fetch($post['author_id']);
+            PostComponent::render($author['name'], $author['logo_url'], $post['content'], $post['media_url'], $post['created_at']);
+        }
+        ?>
+    </main>
+    <div class="right">
+        Right
+    </div>
+</div>
+<?php
 Footer::render();
 ?>
