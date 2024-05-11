08b6472b91a95be9b61ceb84db5e856084dcb47e
Lec <150683104+Leckii@users.noreply.github.com>
2024-05-11 17:24:36 +0800

    event bg image

--- eventspage/eventpage.php
+++ eventspage/eventpage.php
@@ -7,7 +7,7 @@
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- CS -->
-    <link rel="stylesheet" href="eventsyle.css">
+    <link rel="stylesheet" href="eventstyle.css">
     <title>Events</title>
 </head>
 
