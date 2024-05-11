d9cfff0914239d084b2e5da76fdf43e89e292177
Mhai Laine Bringuelo <115721615+MhaiLaine@users.noreply.github.com>
2024-05-11 12:28:04 +0800

    Header and Footer in Separate Files

--- components/header.php
+++ components/header.php
@@ -41,14 +41,6 @@
     </header>
 
 
-
-    <footer>
-        <div class="container-fluid text-center">
-           <p>© Copyright 2024. University of Santo Tomas. All Rights Reserved. </p>
-        </div>
-    </footer>
-
-
 </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
