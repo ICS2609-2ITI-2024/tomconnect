bfb7f621cfb539e5d12e64b5fcd2a93eaaa832fc
Lec <150683104+Leckii@users.noreply.github.com>
2024-05-11 18:10:12 +0800

    updated sass

--- public/eventpage.php
+++ public/eventpage.php
@@ -7,12 +7,17 @@
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- CS -->
-    <link rel="stylesheet" href="eventstyle.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Events</title>
 </head>
 
 <body>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body card-main-style">
+            <h5 class="card-title">Events</h5>
 
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
