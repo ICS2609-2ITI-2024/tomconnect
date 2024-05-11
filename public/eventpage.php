c3a15e5814ee3c146f0623b2895ad0255f045666
Lec <150683104+Leckii@users.noreply.github.com>
2024-05-11 19:18:09 +0800

    event page updated

--- public/eventpage.php
+++ public/eventpage.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
+
+use Tomconnect\Components\EventCard;
+use Tomconnect\Components\EventComponent;
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,11 +19,34 @@
 </head>
 
 <body>
-    <div class="card" style="width: 18rem;">
-        <div class="card-body card-main-style">
+    <div class="card main-card-style" style="width: 18rem;">
+        <div class="card-body card-main-title">
             <h5 class="card-title">Events</h5>
-
         </div>
+        <br>
+        <div class="container-date">
+            <select name="date" id="date">
+                <option value="test">Any date</option>
+            </select>
+            <select name="time" id="time">
+                <option value="test">Any time</option>
+            </select>
+        </div>
+        <br>
+        <table>
+            <td>
+                <div class="container container-event">
+                    <div class="container card card-style">
+                        <h3>EVENT CARD SAMPLE</h3>
+                    </div>
+            </td>
+            <td>
+                <div class="container container-event">
+                    <div class="container card card-style">
+                        <h3>EVENT CARD SAMPLE</h3>
+                    </div>
+            </td>
+        </table>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
