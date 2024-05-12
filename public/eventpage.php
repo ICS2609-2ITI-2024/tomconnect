3947fc2558f04808e8ae7573d83b4d14034a728b
Lec <alecgabriel.estrera.cics@ust.edu.ph>
2024-05-12 18:11:55 +0800

    Event edits
    temporarily linked a seperate style sheet because was unable to link the sass file at the current pc

--- public/eventpage.php
+++ public/eventpage.php
@@ -14,16 +14,15 @@ use Tomconnect\Components\EventComponent;
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- CS -->
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/eventpagestyle.css">
     <title>Events</title>
 </head>
 
 <body>
-    <div class="card main-card-style" style="width: 18rem;">
-        <div class="card-body card-main-title">
-            <h5 class="card-title">Events</h5>
+    <div class="card main-card-style">
+        <div class="card-body">
+            <h6 class="card-title">Events</h6>
         </div>
-        <br>
         <div class="container-date">
             <select name="date" id="date">
                 <option value="test">Any date</option>
@@ -33,20 +32,70 @@ use Tomconnect\Components\EventComponent;
             </select>
         </div>
         <br>
-        <table>
-            <td>
-                <div class="container container-event">
-                    <div class="container card card-style">
-                        <h3>EVENT CARD SAMPLE</h3>
-                    </div>
-            </td>
-            <td>
-                <div class="container container-event">
-                    <div class="container card card-style">
-                        <h3>EVENT CARD SAMPLE</h3>
-                    </div>
-            </td>
-        </table>
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="container card card-style">
+                            <h6 class="title-style">EVENT CARD SAMPLE</h6>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
