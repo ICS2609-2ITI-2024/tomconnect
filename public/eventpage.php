fc225cadf00be97e6890cd301b8e0742397e7577
Lec <150683104+Leckii@users.noreply.github.com>
2024-05-13 18:48:28 +0800

    Added page number navigator and changed the file name of css

--- public/eventpage.php
+++ public/eventpage.php
@@ -11,7 +11,7 @@ require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- CS -->
-    <link rel="stylesheet" href="./css/eventpagestyle.css">
+    <link rel="stylesheet" href="./css/eventpagestyles.css">
     <title>Events</title>
 </head>
 
@@ -92,6 +92,16 @@ require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
                     </td>
                 </tr>
             </table>
+        </div>
+        <div class="pagination">
+            <a href="#">&laquo;</a>
+            <a class="active" href="#">1</a>
+            <a href="#">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>
+            <a href="#">5</a>
+            <a href="#">6</a>
+            <a href="#">&raquo;</a>
         </div>
     </div>
 
