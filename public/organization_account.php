560d7df1a7432f8b1bf97d83b765acfb81c305f0
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-05-14 02:50:00 +0800

    changed organization_account

--- public/organization_account.php
+++ public/organization_account.php
@@ -1,6 +1,9 @@
-<?php 
-    declare(strict_types=1); 
-    require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
+<?php
+
+declare(strict_types=1);
+
+session_start();
+require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
 ?>
 
 <!DOCTYPE html>
@@ -18,13 +21,15 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body> 
-    <?php 
+
+<body>
+    <?php
+
     use Tomconnect\Components\CreatePostComponent;
     use Tomconnect\Components\PostComponent;
     use Tomconnect\Components\Footer;
     use Tomconnect\Components\Header;
-    use Tomconnect\Components\NavbarComponent;    
+    use Tomconnect\Components\NavbarComponent;
 
     use Tomconnect\Controllers\Post;
 
@@ -39,28 +44,32 @@
     /** 
      * Container for Organization's Cover Photo
      * retrieve cover_img_url from organizations database
-    */
-    class CoverPhoto {
-        public static function render($image_url, $org_id) {
-        ?>
+     */
+    class CoverPhoto
+    {
+        public static function render($image_url, $org_id)
+        {
+    ?>
             <div class="card">
                 <img src="<?php echo $image_url ?>" class="card-img-top" alt="..." id="<?= $org_id ?>">
             </div>
-        <?php 
+        <?php
         }
     }
 
     /** 
      * Container for Organization's Profile Photo
      * retrieve logo_url from organizations database
-    */
-    class ProfilePhoto {
-        public static function render($logo_url, $org_id) {
+     */
+    class ProfilePhoto
+    {
+        public static function render($logo_url, $org_id)
+        {
         ?>
             <div class="card">
-                <img src="<?php echo $logo_url?>" class="card-img-top" alt="..." id="<?= $org_id ?>">
+                <img src="<?php echo $logo_url ?>" class="card-img-top" alt="..." id="<?= $org_id ?>">
             </div>
-        <?php 
+    <?php
         }
     }
 
@@ -68,12 +77,15 @@
     /** 
      * Handler of "About" Section
      * retrieve description from organizations database
-    */
-    class About {
-        
-        }
-    
-    ?>    
+     */
+    class About
+    {
+    }
+
+
+    $org = OrganizationModel::fetch(3);
+    echo $org['name'];
+    ?>
 
     <!-- Front End for Organization Account -->
     <!-- Cover Photo -->
@@ -82,7 +94,7 @@
         <div class="row">
             <div class="col-12">
                 <?php
-                    CoverPhoto::render($image_url, $org_id);
+                CoverPhoto::render($org['cover_img_url'], $org['org_id']);
                 ?>
             </div>
         </div>
@@ -90,7 +102,7 @@
         <div class="row">
             <div class="col-4">
                 <?php
-                    ProfilePhoto::render($image_url, $org_id);
+                ProfilePhoto::render($org['cover_img_url'], $org['org_id']);
                 ?>
             </div>
         </div>
@@ -110,65 +122,61 @@
                     </div>
 
                     <div class="row icon-info">
-                    <img src="./assets/Website.png" alt="Website Icon">
+                        <img src="./assets/Website.png" alt="Website Icon">
                     </div>
 
                 </div>
 
                 <div class="col"> <!--col for information-->
-                <!--Retrieve Org's Location-->
+                    <!--Retrieve Org's Location-->
                     <div class="row">
 
                     </div>
-                
-                <!--Retrieve Org's Contact Email-->
-                <div class="row">
-                <h5> Location </h5>
+
+                    <!--Retrieve Org's Contact Email-->
+                    <div class="row">
+                        <h5> Location </h5>
 
                         <p>Contact Email</p>
                     </div>
 
-                <!--Retrieve Org's Website-->
+                    <!--Retrieve Org's Website-->
 
 
-                <!--Add Line // Separator-->
+                    <!--Add Line // Separator-->
 
-                <!--Indicator if Org Registration is Open or Closed-->
+                    <!--Indicator if Org Registration is Open or Closed-->
 
-                <!--Org Registration Button-->
+                    <!--Org Registration Button-->
 
+                </div>
             </div>
-        </div>
 
             <div class="col">
-                   <?php
-                        echo "<h1> About </h1>";
-                        
+                <?php
+                echo "<h1> About </h1>";
+
+                ?>
+
+                <!-- row of posts // last row-->
+                <div class="row">
+                    <h1> Posts </h1>
+                    <?php
+                    CreatePostComponent::render();
+                    foreach (PostModel::fetch_all() as $post) {
+                        $author = OrganizationModel::fetch($post['author_id']);
+                        PostComponent::render($author['name'], $author['logo_url'], $post['content'], $post['media_url'], $post['created_at']);
+                    }
                     ?>
-
-        <!-- row of posts // last row-->
-        <div class="row">
-                <h1> Posts </h1>
-                    <?php 
-                        CreatePostComponent::render();
-                        foreach (PostModel::fetch_all() as $post) {
-                            $author = OrganizationModel::fetch($post['author_id']);
-                            PostComponent::render($author['name'], $author['logo_url'], $post['content'], $post['media_url'], $post['created_at']);
-                        }
-                    ?>
-        
-        </div>
+                </div>
 
 
-    </div>
-    <?php
-    Footer::render();
-    ?>
+            </div>
+            <?php
+            Footer::render();
+            ?>
 
 </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </html>
-
