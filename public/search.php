1856a3e222ed67a643617588fc155d3a30c8b17f
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-05-13 16:41:23 +0800

    fixed search function

--- public/search.php
+++ public/search.php
@@ -1,1 +1,18 @@
 <?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] !== "GET")
+{
+    header("Location: " . "../public/404.php");
+    die();
+}
+
+require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
+
+use Tomconnect\Controllers\search;
+
+$search = new Search();
+
+$result = $search->search();
+
+echo json_encode($result);
