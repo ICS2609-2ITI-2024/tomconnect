72b02807d926292d85260467b94dd064d5b6ea08
Lec <150683104+Leckii@users.noreply.github.com>
2024-05-16 00:58:16 +0800

    event page update

--- public/eventpage.php
+++ public/eventpage.php
@@ -4,6 +4,10 @@
 <?php
 require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
 
+use Tomconnect\Components\CreatePostComponent;
+use Tomconnect\Components\PostComponent;
+use Tomconnect\Models\PostModel;
+use Tomconnect\Models\OrganizationModel;
 use Tomconnect\Components\Footer;
 use Tomconnect\Components\Header;
 use Tomconnect\Components\NavbarComponent;
@@ -15,84 +19,26 @@ NavbarComponent::render();
 
 ?>
 <img src="./assets/ust_landscape.png" alt="" class="back-img">
-<div class="main-card-style">
-    <div>
-        <h6 class="card-title">Events</h6>
-    </div>
-    <div>
-        <select name="date" id="date">
-            <option value="test">Any date</option>
-        </select>
-        <select name="time" id="time">
-            <option value="test">Any time</option>
-        </select>
-    </div>
-    <div>
-        <table>
-            <tr>
-                <td>
-                    <?php
-                    $event1 = EventModel::fetch(1);
-                    EventCard::render($event1['description'], $event1['location'], $event1['link']); ?>
-                </td>
-                <td>
-                    <?php
-                    $event2 = EventModel::fetch(2);
-                    EventCard::render($event2['description'], $event2['location'], $event2['link']); ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="container card card-style">
-                        <h6 class="title-style">EVENT CARD SAMPLE</h6>
-                    </div>
-                </td>
-                <td>
-                    <div class="container card card-style">
-                        <h6 class="title-style">EVENT CARD SAMPLE</h6>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="container card card-style">
-                        <h6 class="title-style">EVENT CARD SAMPLE</h6>
-                    </div>
-                </td>
-                <td>
-                    <div class="container card card-style">
-                        <h6 class="title-style">EVENT CARD SAMPLE</h6>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="container card card-style">
-                        <h6 class="title-style">EVENT CARD SAMPLE</h6>
-                    </div>
-                </td>
-                <td>
-                    <div class="container card card-style">
-                        <h6 class="title-style">EVENT CARD SAMPLE</h6>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <div class="container card card-style">
-                        <h6 class="title-style">EVENT CARD SAMPLE</h6>
-                    </div>
-                </td>
-                <td>
-                    <div class="container card card-style">
-                        <h6 class="title-style">EVENT CARD SAMPLE</h6>
-                    </div>
-                </td>
-            </tr>
-        </table>
+<div>
+    <h6 class="card-title">Events</h6>
+</div>
+<div class="date-container">
+    <select name="date" id="date">
+        <option value="test">Any date</option>
+    </select>
+    <select name="time" id="time">
+        <option value="test">Any time</option>
+    </select>
+</div>
+<?php
+echo '<div class= "events-container" >';
+foreach (EventModel::fetch_all() as $event) {
+    $author = EventModel::fetch($event['event_id']);
+    EventCard::render($event['description'], $event['location'], $event['link']);
+}
+echo '</div>';
+?>
+<?php
 
-    </div>
-    <?php
-
-    Footer::render();
-    ?>
+Footer::render();
+?>
