b37845ee08064b1570fac45635c230c0ab6dd5d5
Lec <alecgabriel.estrera.cics@ust.edu.ph>
2024-05-16 10:17:36 +0800

    updated the event card places

--- public/eventpage.php
+++ public/eventpage.php
@@ -16,28 +16,40 @@ use Tomconnect\Models\EventModel;
 
 Header::render('tomconnect');
 NavbarComponent::render();
+?>
 
-?>
+
 <img src="./assets/ust_landscape.png" alt="" class="back-img">
-<div>
-    <h6 class="card-title">Events</h6>
+<div class="main-feed">
+    <div class="left">
+    </div>
+    <main class="main">
+        <div>
+            <h6 class="title-card">Events</h6>
+        </div>
+        <div class="date-container">
+            <select name="date" id="date">
+                <option value="test">Any date</option>
+            </select>
+            <select name="time" id="time">
+                <option value="test">Any time</option>
+            </select>
+        </div>
+        <?php
+        echo '<div class="events-container">';
+        foreach (EventModel::fetch_all() as $event) {
+            echo '<div class="event-card">';
+            $author = EventModel::fetch($event['event_id']);
+            EventCard::render($event['description'], $event['location'], $event['link']);
+            echo '</div>';
+        }
+        echo '</div>';
+        ?>
+    </main>
+    <div class="right">
+    </div>
 </div>
-<div class="date-container">
-    <select name="date" id="date">
-        <option value="test">Any date</option>
-    </select>
-    <select name="time" id="time">
-        <option value="test">Any time</option>
-    </select>
-</div>
-<?php
-echo '<div class= "events-container" >';
-foreach (EventModel::fetch_all() as $event) {
-    $author = EventModel::fetch($event['event_id']);
-    EventCard::render($event['description'], $event['location'], $event['link']);
-}
-echo '</div>';
-?>
+
 <?php
 
 Footer::render();
