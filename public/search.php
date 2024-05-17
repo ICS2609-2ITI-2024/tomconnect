965132e80c361dc8767e18d9c2fe9077b7316ae2
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-05-17 23:57:57 +0800

    added new component

--- public/search.php
+++ public/search.php
@@ -25,15 +25,19 @@ Header::render('Tomconnect');
 NavbarComponent::render();
 ?>
 <img src="./assets/testbg 2.svg" alt="" class="back-img">
-<main class="center">
-    <div class="">
-        <?php 
+<div class="main-feed">
+    <div class="left">
+    </div>
+    <main class="main">
+        <?php
         foreach ($posts as $post) {
             $author = OrganizationModel::fetch($post['author_id']);
             PostComponent::render($author['name'], $author['logo_url'], $post['content'], $post['media_url'], $post['created_at']);
         }
         ?>
+    </main>
+    <div class="right">
     </div>
-</main>
+</div>
 <?php
 Footer::render();
