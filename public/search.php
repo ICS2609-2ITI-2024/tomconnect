bf83217cb9ec15f39897adf1d70a782409fec2a2
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-05-18 00:00:13 +0800

    refactor: remove unused lines

--- public/search.php
+++ public/search.php
@@ -10,10 +10,11 @@ require_once (dirname(__DIR__)) . "\\vendor\\autoload.php";
 
 use Tomconnect\Components\Footer;
 use Tomconnect\Components\Header;
-use Tomconnect\Components\NavbarComponent;
 use Tomconnect\Controllers\Search;
 use Tomconnect\Components\PostComponent;
 use Tomconnect\Models\OrganizationModel;
+use Tomconnect\Components\NavbarComponent;
+use Tomconnect\Components\OrganizationSearchCardComponent;
 
 $search = new Search();
 
@@ -24,17 +25,31 @@ $organizations = $result['organizations'];
 Header::render('Tomconnect');
 NavbarComponent::render();
 ?>
+<script>
+    const result = <?= json_encode($result) ?>
+    console.table(result);
+</script>
 <img src="./assets/testbg 2.svg" alt="" class="back-img">
 <div class="main-feed">
     <div class="left">
     </div>
     <main class="main">
-        <?php
-        foreach ($posts as $post) {
-            $author = OrganizationModel::fetch($post['author_id']);
-            PostComponent::render($author['name'], $author['logo_url'], $post['content'], $post['media_url'], $post['created_at']);
-        }
-        ?>
+        <div class="orgs">
+            <h2 class="orgs__title">Organizations</h2>
+            <?php 
+            foreach($organizations as $organization) {
+                OrganizationSearchCardComponent::render($organization['name'], $organization['logo_url'], $organization['admin_id'], $organization['description']);
+            }
+            ?>
+        </div>
+        <div class="posts">
+            <?php
+            foreach ($posts as $post) {
+                $author = OrganizationModel::fetch($post['author_id']);
+                PostComponent::render($author['name'], $author['logo_url'], $post['content'], $post['media_url'], $post['created_at']);
+            }
+            ?>
+        </div>
     </main>
     <div class="right">
     </div>
