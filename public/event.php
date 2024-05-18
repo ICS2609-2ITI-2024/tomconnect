ae27a8e6d1b526cd55da9912f4e9acf729c85ebd
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-05-18 16:54:10 +0800

    updated navbar link

--- public/event.php
+++ public/event.php
@@ -14,36 +14,38 @@ Header::render('tomconnect');
 NavbarComponent::render();
 ?>
 <img src="./assets/ust_landscape.png" alt="" class="back-img">
-<div class="main-feed">
+<!-- <div class="main-feed">
     <div class="left">
-    </div>
-    <main class="main">
-        <div>
+    </div> -->
+    <main class="center">
+        <h1>Coming Soon...</h1>
+        <p>Get exclusive early access to our beta page by making a payment through GCash.</p>
+
+        <!-- <div>
             <h6 class="title-card">Events</h6>
         </div>
         <div class="holder">
-        </div>
+        </div> -->
         <?php
-        echo '<div class="events-container">';
-        foreach (EventModel::fetch_all() as $event) {
-            echo '<div class="event-card">';
-            EventCard::render(
-                $event['description'],
-                $event['location'],
-                $event['link'],
-                $event['event_start_date'],
-                $event['event_end_date'],
-                $event['event_time']
-            );
-            echo '</div>';
-        }
-        echo '</div>';
+        // echo '<div class="events-container">';
+        // foreach (EventModel::fetch_all() as $event) {
+        //     echo '<div class="event-card">';
+        //     EventCard::render(
+        //         $event['description'],
+        //         $event['location'],
+        //         $event['link'],
+        //         $event['event_start_date'],
+        //         $event['event_end_date'],
+        //         $event['event_time']
+        //     );
+        //     echo '</div>';
+        // }
+        // echo '</div>';
         ?>
     </main>
-    <div class="right">
-    </div>
+    <!-- <div class="right">
+    </div> -->
 </div>
-
 <?php
 
 Footer::render();
